10349e608a6e9d30cd3e4c26591b447212a06842
JohnCorby <williamforgames6@gmail.com>
2022-03-25 10:30:10 -0700

    harmony

---
+++ VanillaFix/Mod.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace VanillaFix
+{
+    public class Mod : ModBehaviour
+    {
+        public static Mod Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
