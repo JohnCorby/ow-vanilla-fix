f3c63d0cf4b3b5f8600ebc69e917c70b37e1410b
Sander Grindheim <sandsalamand@gmail.com>
2022-07-25 22:04:16 -0700

    Added self-destructor component to created gameObject to make sure it gets destroyed in case QuantumMoon.OnDisable is called in contexts other than exiting to main menu.

--- VanillaFix/QuantumMoonFix.cs
+++ VanillaFix/QuantumMoonFix.cs
@@ -16,6 +16,31 @@ public static class QuantumMoonFix
 		//Solution: if _outerCloudTransform is null, create a new dummy GameObject to sate OnDisable's appetite
 		Transform outerCloudTransform = __instance.GetValue<Transform>("_outerCloudTransform");
 		if (outerCloudTransform == null)
-			__instance.SetValue("_outerCloudTransform", new GameObject().transform);
+		{
+			GameObject fodderGameObject = new GameObject();
+			fodderGameObject.AddComponent<SelfDestructor>();
+			__instance.SetValue("_outerCloudTransform", fodderGameObject.transform);
+		}
+	}
+}
+
+/// <summary>
+/// Destroys the gameObject it's attached to after a few seconds or on disable
+/// </summary>
+public class SelfDestructor : MonoBehaviour
+{
+	private float timer = 0f;
+	public float destructionTime = 3f;
+
+	private void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer > destructionTime)
+			this.gameObject.SetActive(false);
+	}
+
+	private void OnDisable()
+	{
+		Destroy(this.gameObject);
 	}
 }
