857fa40c0070caa755cf90787f42228460ef9e8c
Noah Pilarski <mehmehmeh313@gmail.com>
2022-09-09 20:19:22 -0400

    Add equals button

---
+++ VanillaFix/ButtonPromptFix.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace VanillaFix;
+
+/// <summary>
+/// ButtonPromptLibrary is missing an equals button texture.
+/// this adds one.
+/// </summary>
+[HarmonyPatch(typeof(ButtonPromptLibrary))]
+public static class ButtonPromptFix
+{ 
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(ButtonPromptLibrary.Initialize))]
+    public static void Initialize()
+    {
+        ButtonPromptLibrary.s_keyCodeDict[KeyCode.Equals] = Mod.EqualsButton;
+        ButtonPromptLibrary.s_keyCodeDict[KeyCode.KeypadEquals] = Mod.EqualsButton;
+    }
+}
--- VanillaFix/Mod.cs
+++ VanillaFix/Mod.cs
@@ -1,10 +1,23 @@
 ﻿using HarmonyLib;
 using OWML.ModHelper;
+using System.IO;
 using System.Reflection;
+using UnityEngine;
 
 namespace VanillaFix;
 
 public class Mod : ModBehaviour
 {
+	public static Texture2D EqualsButton { get; private set; }
+
 	private void Awake() => Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+	private void Start()
+	{
+		var texture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+		texture.name = "Keyboard_Black_Equals";
+		texture.wrapMode = TextureWrapMode.Clamp;
+		texture.LoadImage(File.ReadAllBytes(Path.Combine(ModHelper.Manifest.ModFolderPath, "Keyboard_Black_Equals.png")));
+		EqualsButton = texture;
+	}
 }
