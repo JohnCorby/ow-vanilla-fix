e01b89a559622c340b871357005114e3df219cbc
JohnCorby <williamforgames6@gmail.com>
2022-09-15 13:37:52 -0700

    proxy bodies were fixed

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Fixes some vanilla bugs in Outer Wilds
 
 ## Fixes
 - Makes the ship not fall through a planet when you and the probe are not at said planet.
-- Makes distant proxies properly disappear when you open the map.
 - Makes the proxy sun's atmosphere look correct. (credit to MegaPiggy)
 - Properly resets PlayerState on scene change.
 - Adds some missing button prompt textures. (credit to MegaPiggy)
---
+++ VanillaFix/SunProxyFix.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace VanillaFix;
+
+/// <summary>
+/// CREDIT TO MEGAPIGGY FOR THIS FIX
+/// for some reason they do atmosphereScale * 2 in base game, which makes the atmosphere not show up
+/// </summary>
+[HarmonyPatch(typeof(SunProxyEffectController))]
+public static class SunProxyFix
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(SunProxyEffectController.UpdateScales))]
+	private static void UpdateScales(SunProxyEffectController __instance, Vector3 atmosphereScale)
+	{
+		if (__instance._atmosphere)
+			__instance._atmosphere.transform.localScale = atmosphereScale;
+	}
+}
