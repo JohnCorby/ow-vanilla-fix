d6b8c741f97b5878504637ffd4978515b51d30fa
JohnCorby <williamforgames6@gmail.com>
2022-09-16 20:01:05 -0700

    let input work in background lol

---
+++ VanillaFix/BackgroundInputFix.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace VanillaFix;
+
+/// <summary>
+/// patch 13 made it ignore input if the window is not focused.
+/// this breaks Run In Background because it makes keys stay held down when they arent.
+/// </summary>
+[HarmonyPatch(typeof(OWInput))]
+public static class BackgroundInputFix
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(OWInput.Awake))]
+	public static void Awake(OWInput __instance) =>
+		InputSystem.settings.backgroundBehavior = default;
+}
