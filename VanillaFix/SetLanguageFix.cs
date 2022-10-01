ee21ff19fb6554a1c84dcacd398584bb552eca3a
JohnCorby <williamforgames6@gmail.com>
2022-09-30 21:35:37 -0700

    fix language patch

--- VanillaFix/SetLanguageFix.cs
+++ VanillaFix/SetLanguageFix.cs
@@ -11,6 +11,6 @@ public static class SetLanguageFix
 	[HarmonyPrefix]
 	[HarmonyPatch(nameof(TextTranslation.SetLanguage))]
 	public static bool SetLanguage(TextTranslation __instance, TextTranslation.Language lang) =>
-		// don't do language things if we didn't actually change the language
-		__instance.m_language != lang;
+		// don't do language things if we're not in the title screen since that's supposed to be impossible
+		LoadManager.GetCurrentScene() == OWScene.TitleScreen;
 }
