81f7187acd79a7f59ef3a2ab0aeda74c0981bebd
JohnCorby <williamforgames6@gmail.com>
2023-04-23 01:18:02 -0700

    whoops

--- VanillaFix/ProbeNullFixes.cs
+++ VanillaFix/ProbeNullFixes.cs
@@ -6,11 +6,12 @@ namespace VanillaFix;
 /// There are probably a few places the game doesn't check if the probe is null,
 /// but this will house the most common ones.
 /// </summary>
+[HarmonyPatch]
 public static class ProbeNullFixes
 {
 	// NomaiWarpStreaming.FixedUpdate calls this without a null check
 	[HarmonyPrefix]
-	[HarmonyPatch(nameof(SurveyorProbe.IsLaunched))]
+	[HarmonyPatch(typeof(SurveyorProbe), nameof(SurveyorProbe.IsLaunched))]
 	private static bool SurveyorProbe_IsLaunched(SurveyorProbe __instance, out bool __result)
 	{
 		__result = __instance && __instance.gameObject.activeSelf;
