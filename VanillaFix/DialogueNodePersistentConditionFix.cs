4313e53c9d80da6b0226a95b330cf8fbe98833ec
JohnCorby <williamforgames6@gmail.com>
2024-10-27 14:01:38 -0700

    fix #17

---
+++ VanillaFix/DialogueNodePersistentConditionFix.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+
+namespace VanillaFix;
+
+/// <summary>
+/// fixes https://github.com/JohnCorby/ow-vanilla-fix/issues/17
+/// persistent conditions were basically ignored
+/// </summary>
+[HarmonyPatch]
+public static class DialogueNodePersistentConditionFix
+{
+	[HarmonyPrefix, HarmonyPatch(typeof(DialogueNode), nameof(DialogueNode.EntryConditionsSatisfied))]
+	private static bool DialogueNode_EntryConditionsSatisfied(DialogueNode __instance, out bool __result)
+	{
+		bool flag = true;
+		if (__instance._listEntryCondition.Count == 0)
+		{
+			__result = false;
+			return false;
+		}
+		DialogueConditionManager sharedInstance = DialogueConditionManager.SharedInstance;
+		for (int i = 0; i < __instance._listEntryCondition.Count; i++)
+		{
+			string text = __instance._listEntryCondition[i];
+			if (sharedInstance.ConditionExists(text))
+			{
+				if (!sharedInstance.GetConditionState(text))
+				{
+					flag = false;
+				}
+			}
+			// CHANGED: remove the !
+			else if (PlayerData.PersistentConditionExists(text))
+			{
+				if (!PlayerData.GetPersistentCondition(text))
+				{
+					flag = false;
+				}
+			}
+			else
+			{
+				flag = false;
+			}
+		}
+		__result = flag;
+		return false;
+	}
+}
