3a4500d4c9974a7a2355ecc4ab3fa331df7443aa
Spacemike <MWHarlam@Yahoo.com>
2025-01-06 13:02:30 -0500

    Signalscope Waveform UI Fix
    Added new fix to disable part of the Signalscope Waveform UI at the start of the scene to prevent it from floating in space and being visible before the signalscope is pulled out.

---
+++ VanillaFix/SignalscopeUIFix.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace VanillaFix;
+
+/// <summary>
+/// CREDIT TO SPACEMIKE FOR THIS FIX
+///
+/// Part of the waveform UI for the signalscope is active at the start of the scene before the
+/// signalscope is pulled out and ends up just floating in space before you equip the signalscope.
+/// </summary>
+[HarmonyPatch(typeof(SignalscopeUI))]
+public static class SignalscopeUIFix
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(SignalscopeUI.Start))]
+	private static void Start(SignalscopeUI __instance) => __instance.SetWaveformUIActive(false);
+}
