ced2eb9383379f54dcd96559e737c7be3354741a
Etherpod <etherpod@gmail.com>
2025-03-14 14:17:41 -0700

    Noise Sensor detection fix

---
+++ VanillaFix/NoiseSensorDetectionFix.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace VanillaFix;
+
+[HarmonyPatch]
+public class NoiseSensorDetectionFix
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NoiseMaker), nameof(NoiseMaker.GetNoiseOrigin))]
+    public static void FixNoiseMakerOrigin(NoiseMaker __instance, ref Vector3 __result)
+    {
+        // Someone might need to change the origin for a custom feature, making sure this doesn't override that
+        if (__result == __instance._attachedBody.GetCenterOfMass())
+        {
+            __result = __instance._attachedBody.GetWorldCenterOfMass();
+        }
+    }
+}
